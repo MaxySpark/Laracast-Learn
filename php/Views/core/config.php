580048129e55005d7be9ce44ea22c27e4d3700bd
MaxySpark <hello@maxyspark.com>
2018-02-01 07:55:38 +0530

    View

---
+++ php/Views/core/config.php
@@ -0,0 +1,15 @@
+<?php
+
+    return [
+        "database" => [
+            'name' => 'todos',
+            'username' => 'root',
+            'password' => '123for5ve',
+            'connection' => 'mysql:host=localhost',
+            'options' => [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]
+        ]
+    ]
+
+?>
---
+++ php/Views/views/partials/nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>   
+        <li>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/about/culture">About Our Culture</a>
+            <a href="/contact">Contact</a>
+        </li>
+    </ul>
+</nav>
